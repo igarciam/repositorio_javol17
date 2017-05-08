dbe6ad7e2c8e9dc287bd0a6ac68de17e50f5c845
Ignacio Garcia <i.g.molinero@gmail.com>
2017-05-08 02:20:29 +0200

    incluyo dependencia selenium

--- Libreria_IgnacioGarcia_Maven/src/test/java/com/grupoatrium/modelo/AutorTest.java
+++ Libreria_IgnacioGarcia_Maven/src/test/java/com/grupoatrium/modelo/AutorTest.java
@@ -2,11 +2,21 @@ package com.grupoatrium.modelo;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AutorTest {
-
+	String driverPath = "C:/geckodriver/";
+		
 	@Test
 	public void testToString() {
+		System.setProperty("webdriver.gecko.driver", driverPath+"geckodriver.exe");
+		DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette", true);
+		WebDriver driver = new FirefoxDriver(capabilities);
+
+		
 		String nombre = "J.K.Rowling";
 		String nacionalidad = "Gran Breataña";
 		String comentarios = "Escritora y productora de cine británica";
